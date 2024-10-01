2fd7d83e94035ba262b286177ffbe2694865244d
AzadAshurov <azadva-bp217@code.edu.az>
2024-10-01 18:50:15 +0400

    heftenin gunleri

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Threading.Tasks.Sources;
+
+Console.WriteLine("ayi daxil edin");
+string ay = Console.ReadLine();
+if (ay == "mart" || ay == "aprel" || ay == "may")
+{
+    Console.WriteLine("yaz");
+
+
+}
+else if (ay =="iyun"|| ay=="iyul" || ay == "avqust")
+{
+    Console.WriteLine("yay");
+
+    
+}
+else if (ay =="sentyabr"|| ay =="oktyabr" || ay== "noyabr")
+{
+    Console.WriteLine("payiz");
+}
+else
+{
+    Console.WriteLine("qis");
+
+}
+
+
+
+
+
+
+
